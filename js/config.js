aa278c95b19e90ebe27c7c64b580cd8c59129420
dodosiz <t.v.asvestopoulos@gmail.com>
2024-01-06 01:28:32 +0200

    User pool

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
-    cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
-    },
-    api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
+  cognito: {
+    userPoolId: "eu-central-1_SJUpYeclS", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "5cgh1dlfqpa3s42t34asi2u2m1", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: "eu-central-1", // e.g. us-east-2
+  },
+  api: {
+    invokeUrl: "", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+  },
 };
